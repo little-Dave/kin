e30207e69b2e13c82a6d666350530473db9ebf6c
little-Dave <17536424+little-Dave@users.noreply.github.com>
2019-09-07 14:58:00 -0600

    Create PeopleContainer and PersonTile components
    Pass currentUser down from App.vue as userInfo; render PersonTile for each person

---
+++ src/components/PeopleContainer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+
+    <ul v-for="person in userInfo.people">
+      <app-person-tile :person="person"></app-person-tile>
+    </ul>
+
+  </div>
+</template>
+
+
+
+<script>
+  import PersonTile from "./PersonTile.vue"
+
+  export default {
+    props: ["userInfo"],
+    components: {
+      "app-person-tile": PersonTile
+    }
+  }
+</script>
+
+
+
+<style scoped>
+
+</style>
